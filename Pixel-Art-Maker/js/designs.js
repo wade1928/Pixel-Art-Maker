a6bc5124a3783a08063d91d2a383a3fa1190d67c
Ryan <ryanwade1928@gmail.com>
2018-05-24 12:53:02 -0500

    Add makeGrid function

--- Pixel-Art-Maker/js/designs.js
+++ Pixel-Art-Maker/js/designs.js
@@ -1,8 +1,26 @@
 // Select color input
 // Select size input
 
+document.getElementById("sizePicker").addEventListener("submit", function(event) {
+	event.preventDefault();
+});
+
+document.getElementById('sizePicker').addEventListener('submit', function() {
+	const gridHeight = document.getElementById('inputHeight').value;
+	const gridWidth = document.getElementById('inputWidth').value;
+	makeGrid(gridWidth, gridHeight);
+})
+
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-  // Your code goes here!
+function makeGrid(gridWidth, gridHeight) {
+	for (let i = 0; i < gridHeight; i++) {
+		const newRow = document.createElement('tr');
+		const gridTable = document.querySelector('#pixelCanvas');
+		gridTable.appendChild(newRow);
+		for (let j = 0; j < gridWidth; j++) {
+			const newColumn = document.createElement('td');
+			gridTable.appendChild(newColumn);
+		}
+	}
 }
